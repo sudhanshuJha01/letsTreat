e2abb38585879d419984a0c59dadd53e7f54b4e4
sudhanshuJha01 <sudhanshujha883@gmail.com>
2024-06-23 12:12:30 +0530

    new project started

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="grid grid-col-1 sm:grid-cols-3 ">
+        <div className="bg-orange-600">Hello</div>
+        <div className="bg-white">I am Learning</div>
+        <div className="bg-green-600">And Understanding</div>
+      </div>
+    </>
+  );
+}
+
+export default App;
